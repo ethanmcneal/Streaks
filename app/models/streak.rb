255b3bab50d402cdf2c5520d6a62ce8e7c029a39
zyzzyxis <13ezimm@gmail.com>
2021-04-20 14:16:48 -0600

    added owner to streaks, migrate

--- app/models/streak.rb
+++ app/models/streak.rb
@@ -4,7 +4,7 @@ class Streak < ApplicationRecord
     has_many :users, through: :user_streaks
 
     def self.streak_all_comments(streak_id)
-        select('c.info, c.media, c.cheer, c.laugh, c.id as comment_id, u.name as user_name, u.nickname, u.image, u.email, u.wins, u.losses, u.id as user_id, s.name as streak_name, s.timeline, s.description, s.category, s.reward, s.open as pub, s.punishment, s.id as streak_id') 
+        select('c.info, c.media, c.cheer, c.laugh, c.id as comment_id, u.name as user_name, u.nickname, u.image, u.email, u.wins, u.losses, u.id as user_id, s.name as streak_name, s.timeline, s.description, s.category, s.reward, s.open as pub, s.punishment, s.owner, s.id as streak_id') 
         .from('comments as c')
         .joins('inner join users u on u.id = c.user_id
           inner join streaks s on s.id = c.streak_id')
