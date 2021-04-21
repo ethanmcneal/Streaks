53ff5125880644b74fcca723fcf4d6123d21ec9e
Callice Austin <callice.a@gmail.com>
2021-04-21 11:46:53 -0600

    infinite scroll owrk

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,16 +1,16 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user 
   belongs_to :streak
 
   def self.every_comment
-    select('c.info, c.media, c.cheer, c.laugh, c.id as comment_id, u.name as user_name, u.nickname, u.image, u.email, u.wins, u.losses, u.id as user_id, s.name as streak_name, s.timeline, s.description, s.category, s.reward, s.open as pub, s.punishment, s.id as streak_id') 
+    select('c.info, to_char(created_at) c.created_at, c.media, c.cheer, c.laugh, c.id as comment_id, u.name as user_name, u.nickname, u.image, u.email, u.wins, u.losses, u.id as user_id, s.name as streak_name, s.timeline, s.description, s.category, s.reward, s.open as pub, s.punishment, s.id as streak_id') 
     .from('comments as c')
     .joins('inner join users u on u.id = c.user_id
       inner join streaks s on s.id = c.streak_id')
   end
   
   def self.comments_by_user(user_id)
-    select('c.info, c.media, c.cheer, c.laugh, c.id as comment_id, u.name as user_name, u.nickname, u.image, u.email, u.wins, u.losses, u.id as user_id, s.name as streak_name, s.timeline, s.description, s.category, s.reward, s.open as pub, s.punishment, s.id as streak_id') 
+    select('c.info, c.created_at, c.media, c.cheer, c.laugh, c.id as comment_id, u.name as user_name, u.nickname, u.image, u.email, u.wins, u.losses, u.id as user_id, s.name as streak_name, s.timeline, s.description, s.category, s.reward, s.open as pub, s.punishment, s.id as streak_id') 
     .from('comments as c')
     .joins('inner join users u on u.id = c.user_id
       inner join streaks s on s.id = c.streak_id')
